0d9f5de7ccc73fb4805437f359c2529e2054c073
code-with-madhu <73005757+code-with-madhu@users.noreply.github.com>
2021-07-06 15:55:58 +0530

    added sample python file

---
+++ sample.py
@@ -0,0 +1,7 @@
+import sys
+FIRST_NAME = sys.argv[1]
+LAST_NAME = sys.argv[2]
+CITIES = sys.argv[3]
+ABOUT_ME = sys.argv[4]
+VACCINATED = sys.argv[5]
+print("your first name is {} and last name is {}\nyou are from {}\nthis is about me: {}\nMy vaccination status: {}".format(FIRST_NAME,LAST_NAME,CITIES,ABOUT_ME,VACCINATED))
